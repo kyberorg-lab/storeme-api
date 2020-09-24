f19c4a6a77cd28b1ea7b6ff354f9f29664f61204
Alex Muravya <kyberorg@yadev.eu>
2020-09-24 12:13:48 +0300

    CORS

---
+++ src/main/java/eu/yadev/storeme/configurations/CorsConfig.java
@@ -0,0 +1,20 @@
+package eu.yadev.storeme.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Value("${application.cors.origin}")
+	private String corsOrigin;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins(corsOrigin);
+	}
+}
